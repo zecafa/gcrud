bf2d3b719fb66794adc051a20098a4031d51f6b7
Jose Carlos <josecarlos.fernandez@groupalia.com>
2015-03-11 16:38:04 +0100

    add module configuration

--- src/gcrud.js
+++ src/gcrud.js
@@ -1,1 +1,41 @@
 var gcrud = angular.module('gcrud',[])
+
+/**
+ * Default options. 
+ *
+ * @namespace editable-options
+ */
+//todo: maybe better have editableDefaults, not options...
+.value('gcrudOptions', {
+
+   /**
+    * defaultSorting. 
+    * 
+    */  
+    defaultSorting: {},
+   /**
+    * defaultFilter. 
+    * 
+    */  
+    defaultFilter: {},
+   /**
+    * itemsPerPage. 
+    * 
+    */  
+    itemsPerPage: 10,
+   /**
+    * apiResourceName. 
+    * 
+    */  
+    apiResourceName: null,
+   /**
+    * gridCallback. 
+    * callback function to execute when restangular get api response
+    * Example:
+    * function(data) {
+    *   Do something with the response data from restangular
+    * }
+    */
+    gridCallback: null
+
+});
