9d66765b8aae3d67a40dd385b600019ed4b5f6b5
Jose Carlos <josecarlos.fernandez@groupalia.com>
2015-03-19 13:34:41 +0100

    cleanup

--- src/gcrud_detail/directives/gcrud-detail-directive.js
+++ src/gcrud_detail/directives/gcrud-detail-directive.js
@@ -2,7 +2,7 @@ angular.module('gcrud').directive('gcrudDetailDirective', function(){
     return {
         transclude: true,
         templateUrl: '../templates/gcrud-detail-template.html',
-        controller: 'gcrudDetailController.js'
+        controller: 'gcrudDetailController.js',
         restrict: 'E'
     };
 })
