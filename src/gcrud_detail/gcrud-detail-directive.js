00fa2cbdb79165f5a5e5b575d607972147f16511
José Carlos Fernández Amado <jcarlos.fernandez.work@gmail.com>
2015-03-30 17:41:01 +0200

    jshint cleanup

--- src/gcrud_detail/gcrud-detail-directive.js
+++ src/gcrud_detail/gcrud-detail-directive.js
@@ -1,58 +1,59 @@
-angular.module('gcrud').controller('gcrudDetailController', ['$scope', '$element', '$attrs', '$transclude', function($scope, $element, $attrs, $transclude) {
-
-    $scope.cancelItemEdit = function() {
-        if(!!$scope.selectedItem && !$scope.selectedItem.id){
-            $scope.$emit('event:refreshTable');
-        }
-        $scope.editableForm.$cancel();
-    };
-
-    $scope.$on('event:cancelItemEdit', function() {
-        $scope.cancelItemEdit();
-    });
-
-    $scope.$on('event:createItem', function() {
-       $scope.editableForm.$cancel();
-       $scope.editableForm.$show();
-       $scope.editableForm.$setPristine();
-    });
-
-    $scope.saveItem = function() {
-
-        $scope.showError = function(element){
-            alert('error');
-        };
-
-        if (!$scope.selectedItem.id) {
-            var newItem = Restangular.copy($scope.selectedItem);
-            baseItem = Restangular.all($scope.GcrudParams.apiResourceName).post(newItem);
-        }
-        else {
-            baseItem = $scope.selectedItem.put();
-        }
-
-        var doSaveItem = function(data) {
-            //TODO: feedback of ok or ko api response
-        };
-
-        baseItem.then(doSaveItem);
-    };
-
-    $scope.deleteItem = function(item) {
-        Restangular.one($scope.GcrudParams.apiResourceName, item.id).remove().then(function(){
-            $scope.$emit('event:refreshTable');
-        });
-    };
-}])
-;
 angular.module('gcrud').directive('gcrudDetail', function(){
     return {
         transclude: true,
         // templateUrl: 'gcrud/gcrud-detail/templates/gcrud-detail-template.html',
-        template: '<div ng-if="!!selectedItem"> <form class="smart-form" editable-form name="editableForm" onaftersave="saveItem()" ng-controller="gcrudDetailController"> <div ng-transclude></div><footer> <span ng-hide="editableForm.$visible"> <button type="button" class="btn btn-primary btn-danger" ng-click="deleteItem(selectedItem)" > Borrar </button> <button type="button" class="btn btn-primary" ng-click="editableForm.$show()" > Editar </button> </span> <span ng-show="editableForm.$visible"> <button type="button" class="btn btn-primary" ng-disabled="editableForm.$waiting" ng-click="cancelItemEdit()"> Cancelar </button> <button type="submit" class="btn btn-primary" ng-disabled="editableForm.$waiting"> Guardar </button> </span> </footer> </form></div><div ng-if="!!emptyItemList"> <div class="alert alert-block alert-warning"> <h4 class="alert-heading">No hay ningún servicio seleccionado!</h4> Elimina los filtros del listado de servicios para poder visualizar el detalle. </div></div>',
+        scope: {
+            'activeItem': '='
+        },
+        require: '^gcrudGrid', 
+        template: '<div ng-if="!!activeItem"> <form class="smart-form" editable-form name="editableForm" onaftersave="saveItem()" ng-controller="gcrudDetailController"> <div ng-transclude></div><footer> <span ng-hide="editableForm.$visible"> <button type="button" class="btn btn-primary btn-danger" ng-click="deleteItem(activeItem)" > Borrar </button> <button type="button" class="btn btn-primary" ng-click="editableForm.$show()" > Editar </button> </span> <span ng-show="editableForm.$visible"> <button type="button" class="btn btn-primary" ng-disabled="editableForm.$waiting" ng-click="cancelItemEdit()"> Cancelar </button> <button type="submit" class="btn btn-primary" ng-disabled="editableForm.$waiting"> Guardar </button> </span> </footer> </form></div><div ng-if="!!emptyItemList"> <div class="alert alert-block alert-warning"> <h4 class="alert-heading">No hay ningún servicio seleccionado!</h4> Elimina los filtros del listado de servicios para poder visualizar el detalle. </div></div>',
         controller: 'gcrudDetailController',
-        restrict: 'E'
-    };
-})
-;
+        restrict: 'E',
+        link: function($scope, iElm, iAttrs, gcrudGridCtrl) {
 
+            $scope.cancelItemEdit = function() {
+                 if(!!$scope.activeItem && !$scope.activeItem.id){
+                    $scope.$emit('event:refreshTable');
+                }
+                $scope.editableForm.$cancel();
+            };
+
+            $scope.saveItem = function() {
+
+                $scope.showError = function(element){
+                    alert('error');
+                };
+
+                if (!$scope.activeItem.id) {
+                    var newItem = Restangular.copy($scope.activeItem);
+                    baseItem = Restangular.all($scope.GcrudParams.apiResourceName).post(newItem);
+                }
+                else {
+                    baseItem = $scope.activeItem.put();
+                }
+
+                var doSaveItem = function(data) {
+                    //TODO: feedback of ok or ko api response
+                };
+
+                baseItem.then(doSaveItem);
+            };
+
+            $scope.deleteItem = function(item) {
+                Restangular.one($scope.GcrudParams.apiResourceName, item.id).remove().then(function(){
+                    $scope.$emit('event:refreshTable');
+                });
+            };  
+
+            $scope.$on('event:cancelItemEdit', function() {
+                $scope.cancelItemEdit();
+            });
+
+            $scope.$on('event:createItem', function() {
+               $scope.editableForm.$cancel();
+               $scope.editableForm.$show();
+               $scope.editableForm.$setPristine();
+            });                      
+        }
+    };
+});
--- src/gcrud_grid/gcrud-grid-directive.js
+++ src/gcrud_grid/gcrud-grid-directive.js
@@ -1,52 +1,64 @@
-angular.module('gcrud').directive('gcrudGrid', function (NgTableParams, gcrudOptions) {
+angular.module('gcrud').controller('gcrudGridController', ['$scope', 'ngTableParams', 'gcrudOptions', function ($scope, NgTableParams,  gcrudOptions) {
+
+    var page     = $scope.gcrudParams.page || 1;
+    var count    = $scope.gcrudParams.itemsPerPage || gcrudOptions.itemsPerPage;
+    var sorting  = $scope.gcrudParams.defaultSorting || gcrudOptions.defaultSorting;
+    var filter   = $scope.gcrudParams.defaultFilter || gcrudOptions.defaultFilter;
+    var total    = $scope.gcrudParams.total || 0;
+    var counts   = $scope.gcrudParams.counts || [];
+    var getData  = $scope.gcrudParams.getData;
+
+    this.removeFilters = function () {
+        this.tableParams.filter(angular.copy(filter));
+        this.tableParams.sorting(angular.copy(sorting));
+    };
+
+    this.showResetButton = function () {
+        var params = this.tableParams.$params;
+        var defaultFiltersOn = angular.equals(params.filter, filter);
+        var defaultSortingOn = angular.equals(params.sorting, sorting);
+        return !defaultFiltersOn || !defaultSortingOn;
+    };
+
+    this.setActiveItem = function (item) {
+        $scope.activeItem = item;
+    };
+
+    this.getActiveItem = function () {
+        return $scope.activeItem || {};
+    };
+
+    $scope.tableParams = new NgTableParams({
+        page: page, // show first page
+        count: count, // count per page
+        sorting: angular.copy(sorting), // initial sorting
+        filter: angular.copy(filter) // initial filter
+    }, {
+        total: total, // length of data
+        counts: counts,
+        getData: getData
+    });
+
+    $scope.$watch('tableParams.data', function (data) {
+        console.log(data);
+        if (!!data.total) {
+            $scope.selectItem(data[0], false);
+            delete $scope.emptyItemList;
+        } else {
+            delete $scope.selectedItem;
+            $scope.emptyItemList = true;
+        }
+    });
+    return this;
+}]);
+
+angular.module('gcrud').directive('gcrudGrid', function () {
     return {
         priority: 90,
         restrict: 'E',
         // templateUrl: '../templates/gcrud-grid-template.html',
         template: '<p><button id="btn-reload" ng-show="showResetButton()" ng-click="removeFilters()" class="btn btn-default">Eliminar filtros</button></p><div ng-transclude></div><div ng-if="!!emptyItemList"> <div class="alert alert-block alert-warning"> <h4 class="alert-heading">No se han encontrado registros!</h4> Elimina los filtros para poder visualizar la lista </div></div><form class="smart-form widget-body no-padding"> <footer> <button type="button" class="btn btn-primary" ng-click="addItem()"> Agregar </button> </footer></form>',
-        transclude: true,
-        link: function ($scope) {
-            var page     = $scope.gcrudParams.page || 1;
-            var count    = $scope.gcrudParams.itemsPerPage || gcrudOptions.itemsPerPage;
-            // var callback = $scope.gcrudParams.callback || gcrudOptions.gridCallback;
-            var sorting  = $scope.gcrudParams.defaultSorting || gcrudOptions.defaultSorting;
-            var filter   = $scope.gcrudParams.defaultFilter || gcrudOptions.defaultFilter;
-            var total    = $scope.gcrudParams.total || 0;
-            var counts   = $scope.gcrudParams.counts || [];
-            var getData  = $scope.gcrudParams.getData;
-
-            $scope.removeFilters = function () {
-                this.tableParams.filter(angular.copy(filter));
-                this.tableParams.sorting(angular.copy(sorting));
-            };
-
-            $scope.showResetButton = function () {
-                var params = this.tableParams.$params;
-                var defaultFiltersOn = angular.equals(params.filter, filter);
-                var defaultSortingOn = angular.equals(params.sorting, sorting);
-                return !defaultFiltersOn || !defaultSortingOn;
-            };
-            $scope.tableParams = new NgTableParams({
-                page: page, // show first page
-                count: count, // count per page
-                sorting: angular.copy(sorting), // initial sorting
-                filter: angular.copy(filter) // initial filter
-            }, {
-                total: total, // length of data
-                counts: counts,
-                getData: getData
-            });
-
-            $scope.$watch('tableParams.data', function (data) {
-                console.log(data);
-                if (!!data.total) {
-                    $scope.selectItem(data[0], false);
-                    delete $scope.emptyItemList;
-                } else {
-                    delete $scope.selectedItem;
-                    $scope.emptyItemList = true;
-                }
-            });
-        }
+        controller: 'gcrudGridController',
+        transclude: true
     };
 });
